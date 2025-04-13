6a905519cba372f07395e66acc17f72b623ebac7
Luís Carlos Silva Eiras <luiscarloseiras@gmail.com>
2025-04-13 12:37:14 -0300

    Update dsa_app.py

--- dsa_app.py
+++ dsa_app.py
@@ -183,7 +183,7 @@ st.sidebar.markdown("""
 Mais tickers podem ser encontrados aqui: https://stockanalysis.com/list/nasdaq-stocks/
 
 ### Finalidade da App:
-Ações da Nasdaq analisadas em tempo real por Agentes de IA usando DeepSeek através do Groq e infraestrutura Streamlit. 
+Ações da Nasdaq analisadas em tempo real por Agentes de IA usando DeepSeek através do Groq, previsão pelo Prophet e infraestrutura Streamlit. 
 """)
 
 if st.sidebar.button("Suporte"):
